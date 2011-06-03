2e2dfe3b54611329ca1b1089bb5cf2e750830c0f
Robert Pickering <robert@strangelights.com>
2011-06-03 15:01:45 +0200

    Remove curring and overloads

--- src/log4f/LogManager.fs
+++ src/log4f/LogManager.fs
@@ -13,11 +13,11 @@ type Logger(logger: ILog) =
     member __.Debug x = Printf.kprintf logger.Debug x
     member __.Info x = Printf.kprintf logger.Info x
     member __.Warn x = Printf.kprintf logger.Warn x
-    member __.Warn (ex: Exception) x = Printf.kprintf (fun x -> logger.Warn(x, ex)) x
+    member __.Warn ((ex: Exception), x) = Printf.kprintf (fun x -> logger.Warn(x, ex)) x
     member __.Error x = Printf.kprintf logger.Error x
-    member __.Error (ex: Exception) x = Printf.kprintf (fun x -> logger.Error(x, ex)) x
+    member __.Error ((ex: Exception), x) = Printf.kprintf (fun x -> logger.Error(x, ex)) x
     member __.Fatal x = Printf.kprintf logger.Fatal x
-    member __.Fatal (ex: Exception) x = Printf.kprintf (fun x -> logger.Fatal(x, ex)) x
+    member __.Fatal ((ex: Exception), x) = Printf.kprintf (fun x -> logger.Fatal(x, ex)) x
     
 
 module LogManager =
